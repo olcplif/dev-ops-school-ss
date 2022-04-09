295a3dbff1a25379e400c80e1ee3f1ed7c29d6d6
olcplif <oleksandr.lysenko.jr@gmail.com>
2022-04-09 14:59:11 +0300

    edited install.sh

--- hw_4_terraform/install.sh
+++ hw_4_terraform/install.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 apt-get update
-apt-get install -y apache2
-service start apache2
-chkonfig apache2 on
+apt-get install apache2 -y
+#cp /home/ubuntu/upload/index.html /var/www/html
+#chmod 644 /var/www/html/index.html
+systemctl enable apache2
+systemctl start apache2
 echo "<html><h1>Welcome to Apache Web Server</h2><h4>Homework for DevOps spring school * SoftServe *</h4><p>By Oleksandr Lysenko</html>" > /var/www/html/index.html
